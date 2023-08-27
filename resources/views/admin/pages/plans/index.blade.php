a6c96d9b6feb2f0acbe650423b67e7e892ee0a9b
Mark-Production <marktecnico@gmail.com>
2023-08-27 09:18:49 -0700

    Add TailwindCss into the project

--- resources/views/admin/pages/plans/index.blade.php
+++ resources/views/admin/pages/plans/index.blade.php
@@ -1,1 +1,17 @@
-<div class="bg-red-400"></div>planos index
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Stylesheet -->
+        @vite('resources/css/app.css')
+    </head>
+    <body class="antialiased">
+        <div class="bg-red-400">
+            probando
+        </div>
+    </body>
+</html>
